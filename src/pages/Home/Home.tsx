0a0333abc6057fdab693c8beae96fd1ab8a25b53
jordan <jordan.s.ramirez@gmail.com>
2025-02-01 10:26:19 -0500

    loading animation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,8 +4,8 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from ".
 import { FileIcon, FolderIcon, MoreHorizontalIcon, PlusIcon } from "lucide-react"
 import { S3File, S3Breadcrumbs } from "./types/s3File"
 import React from "react"
-import { fetchListOfFiles} from "./util/fetch-list-of-files"
-
+import { fetchListOfFiles } from "./util/fetch-list-of-files"
+import { Skeleton } from "@/components/ui/skeleton"
 // const breadcrumbItems = [
 //   { label: "My Drive", href: "" },
 //   { label: "Dev Folder", href: "dev_folder/" },
@@ -26,28 +26,28 @@ export default function Home() {
   const [areResultsLoading, setAreResultsLoading] = React.useState(false)
   const [breadcrumbItems, setBreadcrumbs] = React.useState<S3Breadcrumbs[]>([{
     label: 'Home',
-    href:''
+    href: ''
   }])
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     const asyncFunc = async () => {
-      if(!areResultsLoading) {
-        setAreResultsLoading(_=>true)
+      if (!areResultsLoading) {
+        setAreResultsLoading(_ => true)
         setFiles(await fetchListOfFiles(currDir))
-        setAreResultsLoading(_=>false)
+        setAreResultsLoading(_ => false)
       }
     }
     asyncFunc()
-  },[currDir])
+  }, [currDir])
 
   function handleFolerFileClick(file: S3File) {
-    if(file.type === 'folder') {
-      const newDir = breadcrumbItems[breadcrumbItems.length - 1].href + file.name+'/'
+    if (file.type === 'folder') {
+      const newDir = breadcrumbItems[breadcrumbItems.length - 1].href + file.name + '/'
       const newCrumb: S3Breadcrumbs = {
         label: file.name,
         href: newDir
       }
-      setBreadcrumbs(e=>[...e,newCrumb])
+      setBreadcrumbs(e => [...e, newCrumb])
       setCurrDir(newDir)
     }
   }
@@ -55,7 +55,7 @@ export default function Home() {
   return (
     <div className="space-y-4 p-3">
       <div className="flex items-center justify-between">
-        <Breadcrumbs areResultsLoading={areResultsLoading} items={breadcrumbItems} setCurrDir={setCurrDir} setBreadcrumbs={setBreadcrumbs}/>
+        <Breadcrumbs areResultsLoading={areResultsLoading} items={breadcrumbItems} setCurrDir={setCurrDir} setBreadcrumbs={setBreadcrumbs} />
         <Button>
           <PlusIcon className="h-4 w-4 mr-2" />
           New Folder
@@ -72,27 +72,49 @@ export default function Home() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {files.map((file) => (
-            <TableRow key={file.name}>
-              <TableCell className="font-medium" onClick={()=>{handleFolerFileClick(file)}}>
-                <div className="flex items-center gap-2">
-                  {file.type === "folder" ? (
-                    <FolderIcon className="h-4 w-4 text-blue-500" />
-                  ) : (
-                    <FileIcon className="h-4 w-4 text-gray-500" />
-                  )}
-                  {file.name}
-                </div>
-              </TableCell>
-              <TableCell>{file.size}</TableCell>
-              <TableCell>{file.modified}</TableCell>
-              <TableCell>
-                <Button variant="ghost" size="sm" className="h-8 w-8 p-0">
-                  <MoreHorizontalIcon className="h-4 w-4" />
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
+          {areResultsLoading ? (
+            <>
+              {Array.from({ length: 5 }).map((_, rowIndex) => (
+                <TableRow key={rowIndex}>
+                  <TableCell className="font-medium">
+                    <div className="flex items-center gap-2">
+                      <Skeleton className="h-4 w-4" />
+                      <Skeleton className="h-4 w-[100px]" />
+                    </div>
+                  </TableCell>
+                  <TableCell><Skeleton className="h-4 w-[100px]" /></TableCell>
+                  <TableCell><Skeleton className="h-4 w-[100px]" /></TableCell>
+                  <TableCell>
+                    <Skeleton className="h-8 w-8" />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </>
+          ) : (
+            <>
+              {files.map((file) => (
+                <TableRow key={file.name}>
+                  <TableCell className="font-medium" onClick={() => { handleFolerFileClick(file) }}>
+                    <div className="flex items-center gap-2">
+                      {file.type === "folder" ? (
+                        <FolderIcon className="h-4 w-4 text-blue-500" />
+                      ) : (
+                        <FileIcon className="h-4 w-4 text-gray-500" />
+                      )}
+                      {file.name}
+                    </div>
+                  </TableCell>
+                  <TableCell>{file.size}</TableCell>
+                  <TableCell>{file.modified}</TableCell>
+                  <TableCell>
+                    <Button variant="ghost" size="sm" className="h-8 w-8 p-0">
+                      <MoreHorizontalIcon className="h-4 w-4" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </>
+          )}
         </TableBody>
       </Table>
     </div>
