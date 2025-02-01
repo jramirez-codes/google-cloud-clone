fee7048742a45d3c24788a1704accaeb8dab9611
jordan <jordan.s.ramirez@gmail.com>
2025-02-01 16:27:28 -0500

    human readable error fix

--- src/pages/Home/util/human-readable-data-size.tsx
+++ src/pages/Home/util/human-readable-data-size.tsx
@@ -1,14 +1,20 @@
 export function humanReadableSize(bytes: number) {
-  if (bytes < 0) throw new Error("Bytes value cannot be negative");
+  try {
 
-  const units = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-  let size = bytes;
-  let unitIndex = 0;
+    if (bytes < 0) throw new Error("Bytes value cannot be negative");
 
-  while (size >= 1024 && unitIndex < units.length - 1) {
-    size /= 1024;
-    unitIndex++;
+    const units = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    let size = bytes;
+    let unitIndex = 0;
+
+    while (size >= 1024 && unitIndex < units.length - 1) {
+      size /= 1024;
+      unitIndex++;
+    }
+
+    return `${size.toFixed(2)} ${units[unitIndex]}`;
   }
-
-  return `${size.toFixed(2)} ${units[unitIndex]}`;
+  catch (_) {
+    return "-"
+  }
 }
